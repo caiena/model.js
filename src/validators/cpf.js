0da9d1a1dba9025d65c3f639949a31b4b274b285
Douglas Henrique <douglas.henrique@caiena.net>
2020-02-06 17:53:06 -0300

    using require on cpf lib

--- src/validators/cpf.js
+++ src/validators/cpf.js
@@ -1,11 +1,12 @@
 import validate from 'validate.js'
 import _        from '@caiena/lodash-ext'
-import { isValid } from '@fnando/cpf'
+// import { isValid } from '@fnando/cpf'
+const { isValid } = require("@fnando/cpf/dist/node")
 
 function cpf(value, options, key, attrs) {
   let opts = _.merge({}, this.options, options)
 
-  if (!isValid(value)) {
+  if (_.present(value) && !isValid(value)) {
     return opts.message || this.message || "is not a valid CPF";
   }
 
